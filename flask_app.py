4bb95bca599e385ef36ae59c4531d4d08de2eb60
n1kkqt <n1kkqt@gmail.com>
2020-06-30 18:04:05 +0300

    changed host

--- flask_app.py
+++ flask_app.py
@@ -60,4 +60,4 @@ def upload_image():
       return jsonify({'error':'File has invalid extension'})
     
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0',debug=True)
+    app.run(debug=True)
